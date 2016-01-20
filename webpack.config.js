8a4de18f41e3a9bf43dff4ed851c38f4f4573b0e
Jian-Huan Hong <jhong@ancestry.com>
2016-01-19 16:28:11 -0800

    Use path module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+var path = require('path');
+
 module.exports = {
-    entry: "./app/main.js",
+    entry: {
+        app: ["./app/main.js"]
+    },
     output: {
-        path: __dirname + '/build',
+        path: path.resolve(__dirname + '/build'),
         filename: "bundle.js"
     },
     module: {
