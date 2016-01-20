cdd507a3d2e71b38a35147d1382e626e2dcd7aa7
Jian-Huan Hong <jhong@ancestry.com>
2016-01-20 11:13:57 -0800

    Add navigation bar.

--- app/screens/App/index.js
+++ app/screens/App/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import Route from 'react-router'
-import HelloBox from  './screens/Hello/HelloBox'
+import { Link, Route } from 'react-router'
 import HelloRoute from './screens/Hello/index'
 import AlbumListRoute from './screens/Albums/index'
 
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-default" role="navigation">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand">React Fun</a>
+                        </div>
+                        <div className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/hello">Hello</Link></li>
+                                <li><Link to="/albums">Albums</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+})
+
 export default (
-    <Route path="/">
+    <Route path="/" component={App}>
         { HelloRoute }
         { AlbumListRoute }
     </Route>
