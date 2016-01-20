4d18732fd034e21ff8e10bed7729987943f81d47
Jian-Huan Hong <jhong@ancestry.com>
2016-01-20 11:51:06 -0800

    Simplify main.js

--- app/main.js
+++ app/main.js
@@ -6,12 +6,7 @@ import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import appRoutes from './screens/App/index'
 
-var renderShell = () => {
-    var shell = document.createElement('div');
-
-    document.body.appendChild(shell);
-    render(<Router history = { browserHistory } routes = { appRoutes } />, shell)
-};
-
-// init shell
-renderShell();
+render(
+    <Router history = { browserHistory } routes = { appRoutes } />,
+    document.getElementById('content')
+)
