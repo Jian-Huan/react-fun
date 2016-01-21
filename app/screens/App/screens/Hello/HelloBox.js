c841d461dfd512761cfec8db8d8fa39187221507
Jian-Huan Hong <jhong@ancestry.com>
2016-01-21 11:45:34 -0800

    Refactor the HelloBox component to a ES6 class.

--- app/screens/App/screens/Hello/HelloBox.js
+++ app/screens/App/screens/Hello/HelloBox.js
@@ -19,26 +19,28 @@ const HelloTextInput = React.createClass({
     }
 })
 
-const HelloBox = React.createClass({
-    getInitialState: function() {
-        return {
+class HelloBox extends React.Component{
+    constructor() {
+        super();
+        this._handleUserInput = this._handleUserInput.bind(this);
+        this.state = {
             inputText: ''
-        }
-    },
-
-    handleUserInput: function(text) {
-        this.setState({inputText: text})
-    },
+        };
+    }
 
     render() {
         return (
             <div className="container">
                 <span>Write some text in textbox: </span>
-                <HelloTextInput inputText={this.state.inputText} onUserInput={this.handleUserInput}/>
+                <HelloTextInput inputText={this.state.inputText} onUserInput={this._handleUserInput}/>
                 <h1>Hello {this.state.inputText}</h1>
             </div>
         )
-     }
- })
+    }
+
+    _handleUserInput(text) {
+        this.setState({inputText: text})
+    }
+}
 
 export default HelloBox
