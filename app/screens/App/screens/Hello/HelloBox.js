0d67ac40bd70037c7acd00df43ab64c4252e585e
Jian-Huan Hong <jhong@ancestry.com>
2016-01-21 10:15:33 -0800

    Display user input.

--- app/screens/App/screens/Hello/HelloBox.js
+++ app/screens/App/screens/Hello/HelloBox.js
@@ -1,33 +1,44 @@
 import React from 'react'
 
+
+// ToDo: convert code to es6
 const HelloTextInput = React.createClass({
+    handleChange: function() {
+        this.props.onUserInput(this.refs.helloTextInput.value)
+    },
+
     render() {
         return (
-            <input type="text" value={this.props.inputText}/>
+            <input
+                type="text"
+                value={this.props.inputText}
+                ref="helloTextInput"
+                onChange={this.handleChange}
+            />
         )
     }
 })
 
 const HelloBox = React.createClass({
-    getInitialState() {
+    getInitialState: function() {
         return {
             inputText: ''
         }
     },
 
+    handleUserInput: function(text) {
+        this.setState({inputText: text})
+    },
+
     render() {
         return (
             <div className="container">
-                Write some text in textbox: <HelloTextInput inputText={this.state.inputText}/>
-                <h1>Hello</h1>
+                <span>Write some text in textbox: </span>
+                <HelloTextInput inputText={this.state.inputText} onUserInput={this.handleUserInput}/>
+                <h1>Hello {this.state.inputText}</h1>
             </div>
         )
      }
  })
 
 export default HelloBox
-
-/*
- <input type="text" ng-model="sometext" />
- <h1>Hello {{ sometext }}</h1>
- */
