56e85b86171b5fbc5589be589bca5861cc617c74
Jian-Huan Hong <jianhuan.hung@gmail.com>
2016-01-20 20:30:48 -0800

    Add HelloTextInput react class.

--- app/screens/App/screens/Hello/HelloBox.js
+++ app/screens/App/screens/Hello/HelloBox.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
-const HelloBox = React.createClass({
+const HelloTextInput = React.createClass({
     render() {
         return (
-            <label>
-                Write some text in textbox:
-            </label>
+            <input type="text" value={this.props.inputText}/>
+        )
+    }
+})
+
+const HelloBox = React.createClass({
+    getInitialState() {
+        return {
+            inputText: ''
+        }
+    },
+
+    render() {
+        return (
+            <div className="container">
+                Write some text in textbox: <HelloTextInput inputText={this.state.inputText}/>
+                <h1>Hello</h1>
+            </div>
         )
      }
- });
+ })
 
 export default HelloBox
 
+/*
+ <input type="text" ng-model="sometext" />
+ <h1>Hello {{ sometext }}</h1>
+ */
